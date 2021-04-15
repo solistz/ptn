27733f758483a3a7bd201b9d6d149e3a1c4ab8ad
solistz <zhydan@gmail.com>
2021-04-15 12:19:46 +0300

    HW4 DB 0001

---
+++ db.py
@@ -0,0 +1,42 @@
+# Написать базу данных, где записи это массив словарей
+
+
+#while True:
+def Controller():
+    a = ('\n'
+    '| 1 - Create '
+    '| 2 - Modify '
+    '| 3 - Remove '
+    '| 4 - View Dictionary |'
+    '\n'
+    )
+    print(a)
+
+
+
+def Create_dict_el():
+    print('CREATE')
+
+def Modify_dict_el():
+    print('MODIFY')
+    
+def Remove_dict_el():
+    pass
+def View_dict_el():
+    pass
+
+def input_dict_el():
+    print('ВВедіть число 1 - 4 : ')
+    number = int(input())
+
+
+
+def main():
+    dictionary_master = {}
+    all_function = {Create_dict_el(), Modify_dict_el(), Remove_dict_el(), View_dict_el()}
+
+    Controller()
+    input_dict_el()
+
+if __name__ == "__main__":
+    main()
