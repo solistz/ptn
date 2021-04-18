c19dac2751162ee9b297b1121a314c1b7c072169
solistz <zhydan@gmail.com>
2021-04-18 16:26:27 +0300

    2021-04-18 - array->dict->key modify ERROR

--- db.py
+++ db.py
@@ -2,7 +2,7 @@
 
 
 #while True:
-def Controller():
+def controller():
     a = ('\n'
     '| 1 - Create '
     '| 2 - Modify '
@@ -12,32 +12,36 @@ def Controller():
     )
     print(a)
 
-
-
-def Create_dict_el():
-    print('CREATE')
-
-def Modify_dict_el():
-    print('MODIFY')
-    
-def Remove_dict_el():
-    pass
-def View_dict_el():
-    print('hellow')
-
-def input_dict_el():
+def input_dictionary_element():
     print('ВВедіть число 1 - 4 : ')
     number = int(input())
+    return number
 
-# test commit
+
+def create_dictionary_element():
+    print('CREATE')
+
+def modify_dictionary_element():
+    print('MODIFY')
+    
+def remove_dictionary_element():
+    pass
+
+def view_dictionary_element():
+    print('hellow')
+
+def all_function_controller():
+    pass
+
+
+
 
 
 def main():
-    print('test commit')
-    dictionary_master = {}
-    all_function = {Create_dict_el(), Modify_dict_el(), Remove_dict_el(), View_dict_el()}
-
-    Controller()
-    View_dict_el()
+    #dictionary_master = {}
+    controller()
+    number_function = input_dictionary_element()
+    print(number_function, type(number_function))
 if __name__ == "__main__":
     main()
+
---
+++ for-array-dict.py
@@ -0,0 +1,10 @@
+db_array = [{1:1},{2:2},{3:3},{4:4},{5:5},{6:6},{7:7}]
+dictionary_key = int(input())
+for index in db_array:
+    for f,d in index.items():
+        if f == dictionary_key:
+            print(db_array[f-1])
+            # db_array[f-1[dictionary_key] = 90]
+            print(db_array[f-1[dictionary_key] = 22])
+
+        
