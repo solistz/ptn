50f9dd959eb61baaa172dbeb7a1ef2fb354710d9
solistz <zhydan@gmail.com>
2021-04-20 02:36:42 +0300

    for не удаляется1

--- for-array-dict.py
+++ for-array-dict.py
@@ -1,10 +1,13 @@
 db_array = [{1:1},{2:2},{3:3},{4:4},{5:5},{6:6},{7:7}]
 dictionary_key = int(input())
 for index in db_array:
-    for f,d in index.items():
-        if f == dictionary_key:
-            print(db_array[f-1])
-            # db_array[f-1[dictionary_key] = 90]
-            print(db_array[f-1[dictionary_key] = 22])
+    print(index)
+    if dictionary_key == 
+# for index in db_array:
+#     for f,d in index.items():
+#         if f == dictionary_key:
+#             print(db_array[f-1])
+#             # db_array[f-1[dictionary_key] = 90]
+#             # print(db_array[f-1[dictionary_key] = 22])
 
         
