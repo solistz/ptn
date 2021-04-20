4fd99acd8afbe0280d663fa42ad4e28090c6a037
solistz <zhydan@gmail.com>
2021-04-20 04:21:07 +0300

    4-00 ffff

--- for-array-dict.py
+++ for-array-dict.py
@@ -1,7 +1,17 @@
 db_array = [{1:11},{3:3},{4:4},{5:5},{6:6},{7:7}]
 print(db_array)
-dictionary_key = int(input('Key'))
-dictionary_value = int(input('Value'))
+while True:
+    try:
+        dictionary_key = input('Key')
+        if dictionary_key != '':
+            break
+    except ValueError:
+        print('Ви ввели : ', dictionary_key,' спробуйте ще раз')
+while True:
+    dictionary_value = input('Value')
+    if int(dictionary_value) == int(dictionary_value):
+        dictionary_value = int(dictionary_value)
+        break
 iteration = 0
 for index in db_array:
     for f,d in index.items():
@@ -9,6 +19,7 @@ for index in db_array:
             print(db_array[iteration])
             db_array[iteration][dictionary_key] = dictionary_value
             print(db_array[iteration][dictionary_key])
+            dictionary_key = 0
     iteration += 1
-print(iteration)
 print(db_array)
+print(dictionary_key)
