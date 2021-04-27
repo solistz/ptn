67bf491ab7fe64a6cc63b5a8dff081bbafe46da8
solistz <zhydan@gmail.com>
2021-04-27 15:31:53 +0300

    HWork zodiak - works-alpha

--- HW05_zodiak.py
+++ HW05_zodiak.py
@@ -3,8 +3,7 @@
 # а на выходе получается знак зодиака 
 # (постараться сделать без if с помощью словаря!)*
 
-mounth = {1:31,2:28,3:31,4:30,5:31,6:30,7:31,8:31,9:30,10:31,11:30,12:31}
-# mounth = [31,28,31,30,31,30,31,31,30,31,30,31]
+# mounth = {1:31,2:28,3:31,4:30,5:31,6:30,7:31,8:31,9:30,10:31,11:30,12:31}
 
 # zodiak = [
 #     {'vodolei':{1:21,2:18}},
@@ -21,40 +20,58 @@ mounth = {1:31,2:28,3:31,4:30,5:31,6:30,7:31,8:31,9:30,10:31,11:30,12:31}
 #     {'kozerog':{12:22,1:19}}
 #     ]
 
-zodiak = [
-    {'vodolei':[{1:21},{2:18}]},
-    {'fish':[{2:19},{3:20}]},
-    {'oven':[{3:21},{4:20}]},
-    {'telez':[{4:21},{5:20}]},
-    {'bliznez':[{5:21},{6:20}]},
-    {'rak':[{6:21},{7:22}]},
-    {'lev':[{7:23},{8:22}]},
-    {'diva':[{8:23},{9:23}]},
-    {'vagi':[{9:24},{10:23}]},
-    {'skorpion':[{10:24},{11:21}]},
-    {'strilez':[{11:22},{12:21}]},
-    {'kozerog':[{12:22},{1:19}]}
+# zodiak = [
+#     {'vodolei':[{1:21},{2:18}]},
+#     {'fish':[{2:19},{3:20}]},
+#     {'oven':[{3:21},{4:20}]},
+#     {'telez':[{4:21},{5:20}]},
+#     {'bliznez':[{5:21},{6:20}]},
+#     {'rak':[{6:21},{7:22}]},
+#     {'lev':[{7:23},{8:22}]},
+#     {'diva':[{8:23},{9:23}]},
+#     {'vagi':[{9:24},{10:23}]},
+#     {'skorpion':[{10:24},{11:21}]},
+#     {'strilez':[{11:22},{12:21}]},
+#     {'kozerog':[{12:22},{1:19}]}
 
+# ]
+user_mounth = 5
+user_days = 25
+
+
+mounth = [31,28,31,30,31,30,31,31,30,31,30,31]
+ar = 1
+for itm0 in range(len(mounth)):
+    # print(itm0)
+    ar += mounth[itm0]
+    if user_mounth == itm0:
+        print(ar)
+        break 
+ar += user_days
+print(ar)
+
+
+zodiak = [
+    {'vodolei':[21,49]},
+    {'fish':[50,80]},
+    {'oven':[81,111]},
+    {'telez':[112,141]},
+    {'bliznez':[142,172]},
+    {'rak':[173,204]},
+    {'lev':[205,235]},
+    {'diva':[236,267]},
+    {'vagi':[268,297]},
+    {'skorpion':[298,326]},
+    {'strilez':[327,356]},
+    {'kozerog':[357,365]}
 ]
 
-user_mounth = 7
-user_days = 25
-# b = 0
-# for a in mounth:
-#     # print(a)
-#     if a == user_mounth:
-#         print(b)
-#         break
+for itm1 in zodiak:
+    for itm2_i,itm2_v in itm1.items():
+        for itm3 in range(len(itm2_v)):
+            if ar > itm2_v[0] and ar < itm2_v[1]:
+                print('ok',itm2_i)
+                break
 
-for zodiak_elem_dict in zodiak:
-    # print(item)
-    for zodiak_index,zodiak_value in zodiak_elem_dict.items():
-        # print(bb)
-        for zodiak_value_array in zodiak_value:
-            # print(zodiak_value[0])
-            for zodiak_value_array_index,zodiak_value_array_value in zodiak_value_array.items():
-                if zodiak_value_array_index==user_mounth:
-                    print(zodiak_index, zodiak_value_array)
-
-
-
+print('end')
+                    
