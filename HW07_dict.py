2da765b33b8f26333b2fabc5246c91abd4245b94
solistz <zhydan@gmail.com>
2021-05-25 00:51:43 +0300

    feature/create_class - zod 2

--- HW07_dict.py
+++ HW07_dict.py
@@ -11,7 +11,7 @@ class Zodiak_class:
         self._mounth = _mounth
 
     def met_zodiak(self):
-        zodiak = [
+        self.zodiak = [
             {'vodolei':[21,49]},
             {'fish':[50,80]},
             {'oven':[81,111]},
@@ -26,22 +26,30 @@ class Zodiak_class:
             {'kozerog':[357,365]},
             {'kozerog':[1,20]}
         ]
+        return(self.zodiak)
 
     def mounth_method(self):
         mounth = [31,28,31,30,31,30,31,31,30,31,30,31]
+
+    # def met_sum_
 
     def met_print(self):
         print(self._days)
         print(self._mounth)
 
-    def met_print_zodiak(self):
-        # for item_zodiak in self.
-        pass
+    # def met_print_zodiak(self):
+    #     for item_zodiak in self.:
+    #         for m,g in item_zodiak.items():
+    #             for item_g in g:
+    #                 # if item_g[0] >= 
+    #                 print(item_g[0])
+
 
 
 def main():
     obj_zodiak = Zodiak_class(25,7)
     # obj_zodiak.met_print()
+    obj_zodiak.met_zodiak()
 
 
 
